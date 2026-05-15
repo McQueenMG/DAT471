a83ac221c849831d9e2634cdc05529560bc7c63e
MCQueenMG <melkergu@chalmers.se>
2026-05-15 11:32:05 +0200

    Ass5 prob1a done
    Co-authored-by: p0ntu2 <p0ntu2@users.noreply.github.com>

---
+++ Assignment5/Problem1/assignment5_problem1.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#SBATCH -t 00:30:00
+#SBATCH -p short
+#SBATCH -J climate_analysis
+#SBATCH --output=assignment5prob1_%j.log
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+
+base_dir="${SLURM_SUBMIT_DIR:-$PWD}"
+
+containerpath="/data/courses/2026_dat471_dit066/containers/assignment5.sif"
+seed=0x9747b28c
+key=test
+
+apptainer exec \
+  "${containerpath}" \
+  python3 "${base_dir}/assignment5_problem1.py" -s "${seed}" "${key}"
+
+cp "assignment5prob1_${SLURM_JOB_ID}.log" "${base_dir}/output/assignment5prob1-${data_size}-${SLURM_CPUS_PER_TASK}.out"
